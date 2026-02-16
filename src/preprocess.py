ca7cc1b949875721d35bbaf0e7a1fa05262f72cd
sanjaraT <sanjaratunola@gmail.com>
2026-02-16 12:13:11 +0600

    Model training

--- src/preprocess.py
+++ src/preprocess.py
@@ -35,13 +35,13 @@ def balance_data(X_train, y_train):
     smote = SMOTE(random_state=42)
     X_res, y_res = smote.fit_resample(X_train, y_train)
 
-    plt.figure(figsize=(6,4))
-    y_res.value_counts().plot(kind='bar', color=['skyblue', 'salmon'])
-    plt.title("Class Distribution After SMOTE")
-    plt.xlabel("Class")
-    plt.ylabel("Count")
-    plt.xticks(rotation=0)
-    plt.show()
+    # plt.figure(figsize=(6,4))
+    # y_res.value_counts().plot(kind='bar', color=['skyblue', 'salmon'])
+    # plt.title("Class Distribution After SMOTE")
+    # plt.xlabel("Class")
+    # plt.ylabel("Count")
+    # plt.xticks(rotation=0)
+    # plt.show()
 
     return X_res, y_res
 
